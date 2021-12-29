4402cbf2f6bc881240a9135db94ce47b3d1da0c3
TalhaAbid <talhaabid1234@gmail.com>
2021-12-29 16:03:30 -0500

    starting getting puuid for each summoner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import useSwr from "swr";
-import { leaguesResponseType } from "../types";
-import { summonersFetcher } from "./apiHelpers";
+import { useState } from "react";
 import Rankings from "@components/Rankings";
 import Layout from "@components/Layout";
 
